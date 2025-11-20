25f8e9005daa4b9ea8c22df1bbb8e843918ebdc2
prabesh <sitaulaprabesh80@gmail.com>
2025-11-20 16:38:26 +0545

    HWELL

---
+++ HELICOPTER.java
@@ -0,0 +1,12 @@
+public class HELICOPTER{
+  public static void main(String[] args){
+  long l=100000000000L;
+  float f=50.67f;
+  char c='\u2764';
+  System.out.println(l);
+  System.out.println(f);
+  System.out.println();
+
+}
+
+}
--- chess.java
+++ chess.java
@@ -1,17 +1,33 @@
-public class chess{
-    byte b;
-    short s;
-    int i;
-    long l;
-    double d;
-    char c;
-    boolean bo;
-    float f;
+public class chess {
+  byte b;
+short s;
+int i;
+long l;
+double d;
+char c;
+boolean bo;
+float f;
+
+public static void main(String[] args) {
+    chess value=new chess();
+    System.out.println(value.b);
+    System.out.println(value.s);
+    System.out.println(value.i);
+    System.out.println(value.l);
+    System.out.println(value.d);
+    System.out.println(value.c);
+    System.out.println(value.bo);
+    System.out.println(value.f);
+    /*
+     * java doesnt assign values to local variables by default it only assigns to global variables if we try to run by assigning to local variables
+     than it throws compilation error.
+      
     
-        
-    public static void main(String[] args){
-        Defaultvaluechecker value= new defaultvaluechecker();
-        
-        
-    }
+     */
 }
+}
+
+
+
+
+
